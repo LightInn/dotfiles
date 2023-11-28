cc03bffa94a36e83abdac78b4aca595a9aa7eb23
brlef <breval.lefloch@rca.fr>
2023-11-28 12:06:57 +0100

    test

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
 vim.opt.whichwrap:append("<,>,[,],h,l")
+vim.api.nvim_set_keymap("n", "<Down>", [[(line('.') == line('$')) ? 'G' : 'j']], { noremap = true, expr = true })
+vim.api.nvim_set_keymap("n", "<Up>", [[(line('.') == 1) ? 'gg' : 'k']], { noremap = true, expr = true })
