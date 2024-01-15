aea72cafea64b79fbc8301bc69426b4832b204a5
brlef <breval.lefloch@rca.fr>
2024-01-15 17:11:34 +0100

    repasser a tmux <3

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,7 @@
-# The following snippet is meant to be used like this in your fish config:
-#
 if status is-interactive
-    # Configure auto-attach/exit to your likings (default is off).
-    # set ZELLIJ_AUTO_ATTACH true
-    set ZELLIJ_AUTO_EXIT true
-    eval (zellij setup --generate-auto-start fish | string collect)
+    and not set -q TMUX
+    exec tmux
+    # Commands to run in interactive sessions can go here
 end
-if not set -q ZELLIJ
-    if test "$ZELLIJ_AUTO_ATTACH" = "true"
-        zellij attach -c
-    else
-        zellij
-    end
 
-    if test "$ZELLIJ_AUTO_EXIT" = "true"
-        kill $fish_pid
-    end
-end
+starship init fish | source
